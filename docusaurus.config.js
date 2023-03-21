c0082b3558821b0987520fab8a1bc0ef043a9de6
idrisschebak <idrisschebak@me.com>
2023-03-21 23:43:20 +0100

    update module export

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   favicon: "img/favicon.ico",
   deploymentBranch:"main",
   organizationName: "kitsec-labs", // Usually your GitHub org/user name.
-  projectName: "kitsec.github.io", // Usually your repo name.
+  projectName: "website", // Usually your repo name.
   stylesheets: [
     // "https://fonts.google.com/specimen/Montserrat",
     // "https://fonts.googleapis.com/css2?family=Inter:wght@400;700;800&display=block",
